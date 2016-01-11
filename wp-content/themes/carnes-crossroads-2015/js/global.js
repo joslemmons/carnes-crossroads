413ef293701f06648001439dea7743270d966f4f
joslemmons <admin@joslemmons.com>
2016-01-11 17:21:33 -0500

    removed bxslider from small slideshows; added slick.js
    they work now; need to correct carousels the same

--- wp-content/themes/carnes-crossroads-2015/js/global.js
+++ wp-content/themes/carnes-crossroads-2015/js/global.js
@@ -16,26 +16,20 @@ jQuery(function ($) {
         });
     });
 
-    $.each($('div.slider-sm'), function (i, el) {
-        $(el).bxSlider({
-            mode: 'fade',
-            slideWidth: 800,
-            minSlides: 1,
-            maxSlides: 1,
-            moveSlides: 1,
-            slideMargin: 0,
-            pager: false,
-            nextSelector: $(el).parent().find('span.next-sm'),
-            prevSelector: $(el).parent().find('span.prev-sm'),
-            nextText: '',
-            prevText: ''
-        });
+    $('.slider-sm').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        arrows: true,
+        prevArrow: '<span class="prev-sm"></span>',
+        nextArrow: '<span class="next-sm"></span>',
+        cssEase: 'linear'
     });
 
     $('.expand-link').click(function () {
         $(this).hide();
         $(this).closest('div.child-content').find('span.more-content').show();
     });
-
 
 });
--- wp-content/themes/carnes-crossroads-2015/page.php
+++ wp-content/themes/carnes-crossroads-2015/page.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 
 $context = Timber::get_context();
 $context['page'] = Timber::get_post(false, '\App\Model\Page');
+
+wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
+wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
 
 Timber::render('page-default.twig', $context);
