cb286224ec22f4d83d244a808197af7a55a89d39
joslemmons <admin@joslemmons.com>
2016-01-18 15:49:14 -0500

    category list page complete

--- wp-content/themes/carnes-crossroads-2015/category.php
+++ wp-content/themes/carnes-crossroads-2015/category.php
@@ -1,5 +1,10 @@
 <?php
 
 $context = Timber::get_context();
+$context['page'] = Timber::get_post(false, '\App\Model\Page');
 
-Timber::render('news.twig', $context);
+wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
+wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
+
+Timber::render('category.twig', $context);
+
