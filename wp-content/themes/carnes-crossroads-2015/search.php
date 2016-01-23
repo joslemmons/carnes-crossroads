46bd5c042d614fe503630159020b8b6da9ed6e5e
Jonathan Mayhak <jonathan.mayhak@chernoffnewman.com>
2016-01-23 11:34:06 -0500

    set up search page and ability to search from footer

--- wp-content/themes/carnes-crossroads-2015/search.php
+++ wp-content/themes/carnes-crossroads-2015/search.php
@@ -1,5 +1,12 @@
 <?php
 
+global $s;
+
 $context = Timber::get_context();
+
+$context['search_term'] = $s;
+$context['posts'] = Timber::get_posts();
+$context['pagination'] = Timber::get_pagination();
+$context['page'] = Timber::get_post();
 
 Timber::render('search.twig', $context);
