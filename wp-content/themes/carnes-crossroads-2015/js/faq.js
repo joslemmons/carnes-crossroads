4309740d34cbf903a7643e07efb53406f118e0be
joslemmons <admin@joslemmons.com>
2016-01-26 20:23:32 -0500

    faq page styled

--- wp-content/themes/carnes-crossroads-2015/js/faq.js
+++ wp-content/themes/carnes-crossroads-2015/js/faq.js
@@ -1,5 +1,15 @@
-jQuery(function ($) {
-    $('ul').on('click', 'span.faq-question', function() {
-        $(this).parent().find('span.faq-answer').slideToggle();
+jQuery(document).ready(function($) {
+    $('ul.faq-set .faq-question').on('click', function () {
+        $(this).parent().find('span.faq-answer').slideToggle({
+            easing: 'swing',
+            start: function () {
+                if ($(this).parent().find('img').attr('src').indexOf('minus') > -1) {
+                    $(this).parent().find('img').attr('src', '/wp-content/themes/carnes-crossroads-2015/img/plus-circle_360.png');
+                }
+                else {
+                    $(this).parent().find('img').attr('src', '/wp-content/themes/carnes-crossroads-2015/img/minus-circle_360.png');
+                }
+            }
+        });
     });
 });
