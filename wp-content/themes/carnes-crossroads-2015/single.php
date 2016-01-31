71b8bdea33dbde2dcb5a39c0c44ac31ca511e3e0
Jonathan Mayhak <jonathan.mayhak@chernoffnewman.com>
2016-01-31 14:13:08 -0500

    change single.php to show recent sales activity post if chosen
    [deploy:staging]

--- wp-content/themes/carnes-crossroads-2015/single.php
+++ wp-content/themes/carnes-crossroads-2015/single.php
@@ -1,7 +1,19 @@
 <?php
 
-$context = Timber::get_context();
-$context['post'] = Timber::get_post(false, '\App\Model\Post');
-$context['categories'] = Timber::get_terms('category');
+use App\Model\Config;
+use App\Model\Post;
 
-Timber::render('news-single.twig', $context);
+$context = Timber::get_context();
+/* @var Post $post */
+$post = $context['post'] = Timber::get_post(false, '\App\Model\Post');
+
+if ($post->isRecentActivityPost()) {
+    require('tpl-recent-activity-post.php');
+} else {
+    if (in_category('new-offerings', $post->id)) {
+        require('tpl-new-offering.php');
+    } else {
+        $context['categories'] = Timber::get_terms('category');
+        Timber::render('news-single.twig', $context);
+    }
+}
