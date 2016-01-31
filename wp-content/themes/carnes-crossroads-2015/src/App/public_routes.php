271869d4892a9351fd11cfebfddacd90eaa45584
Jonathan Mayhak <jonathan.mayhak@chernoffnewman.com>
2016-01-31 16:51:13 -0500

    set up SEO redirects
    [deploy:staging]

--- wp-content/themes/carnes-crossroads-2015/src/App/public_routes.php
+++ wp-content/themes/carnes-crossroads-2015/src/App/public_routes.php
@@ -11,3 +11,23 @@ function redirectThisToThat($this_url, $that_url)
 Routes::map('/category/all', function () {
     Routes::load('category.php');
 });
+
+// SEO redirects from old site
+redirectThisToThat('/search/', '/home-finder/featured-listings/');
+redirectThisToThat('/search/page/:page/?display-imap', '/home-finder/');
+redirectThisToThat('/builder/', '/homes/builders/');
+redirectThisToThat('/town/commercial-opportunities/', '/community/');
+redirectThisToThat('/whats-difference-master-planned-community-subdivision/', '/community/');
+redirectThisToThat('/carnes-crossroads-real-estate/faqs/', '/faq/');
+redirectThisToThat('/whats-happening-green-barn/', '/lifestyle/');
+redirectThisToThat('/charleston-area/summerville-goose-creek/', '/location/');
+redirectThisToThat('/charleston-area/', '/location/');
+redirectThisToThat('/about-carnes-crossroads-charleston/history/', '/location/');
+redirectThisToThat('/events/all-events/', '/news-events/');
+
+Routes::map('/property/:address/pbase/:property_base_id/', function ($params) {
+    $address = sanitize_text_field($params['address']);
+    $propertyBaseId = sanitize_text_field($params['property_base_id']);
+
+    wp_redirect(home_url() . '/home-finder/properties/' . $address . '/pb_' . $propertyBaseId . '/');
+});
