04624bc81fcb4453564fee3d3bde6e654b489c9d
joslemmons <admin@joslemmons.com>
2016-02-04 11:27:41 -0500

    page layer sliders working

--- wp-content/themes/carnes-crossroads-2015/page-homes.php
+++ wp-content/themes/carnes-crossroads-2015/page-homes.php
@@ -3,4 +3,7 @@
 $context = Timber::get_context();
 $context['page'] = Timber::get_post(false, '\App\Model\HomesPage');
 
+wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
+wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
+
 Timber::render('page-homes.twig', $context);
