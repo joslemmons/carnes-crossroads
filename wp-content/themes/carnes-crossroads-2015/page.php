f233a187333bd7ad122bb31c09496af2c4c8c458
Joseph Lemmons <joseph.lemmons@chernoffnewman.com>
2016-11-10 10:57:16 -0500

    testing Outdated Browser script

--- wp-content/themes/carnes-crossroads-2015/page.php
+++ wp-content/themes/carnes-crossroads-2015/page.php
@@ -5,6 +5,7 @@ $context['page'] = Timber::get_post(false, '\App\Model\Page');
 
 wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
 wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
+wp_enqueue_script('outdated-browser-js', get_template_directory_uri() . '/js/lib/outdatedbrowser/outdatedbrowser.min.js', array('jquery'), false, false);
+wp_enqueue_style('outdated-browser-css', get_template_directory_uri() . '/js/lib/outdatedbrowser/outdatedbrowser.min.css', array(), false, false);
 
 Timber::render('page-default.twig', $context);
-
