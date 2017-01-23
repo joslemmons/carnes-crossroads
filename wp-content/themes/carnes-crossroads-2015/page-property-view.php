d6df12e2b60425575564ee953d163c18ba42a99e
Jeff <jeff.colfer@chernoffnewman.com>
2017-01-23 17:55:37 -0500

    Feature: added single property route and page

--- wp-content/themes/carnes-crossroads-2015/page-property-view.php
+++ wp-content/themes/carnes-crossroads-2015/page-property-view.php
@@ -15,6 +15,16 @@ $context['result'] = $new_offerings_result;
 $context['isNewOfferings'] = true;
 $context['listingsTitle'] = 'New Offerings';
 
+$back_url = $params['http_referer'];
+$need_search = 'real-estate/home-finder/search-listings';
+$need_featured = 'real-estate/home-finder/featured-listings';
+if (is_null($back_url) || ((strpos($back_url,$need_search) === FALSE &&
+        strpos($back_url,$need_featured) === FALSE)))
+{
+    $back_url = '/home-finder';
+}
+$context['back_url'] = $back_url;
+
 HomeFinderPage::enqueueAssets();
 
 Timber::render('page-property-view.twig', $context);
