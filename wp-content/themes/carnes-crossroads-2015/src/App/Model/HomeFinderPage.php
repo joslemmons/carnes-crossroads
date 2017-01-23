5f59b80da4011eef42d3570dd6b09295895284a9
Joseph Lemmons <joseph.lemmons@chernoffnewman.com>
2017-01-23 14:22:41 -0500

    homefinder filters edit; dequeue scripts;

--- wp-content/themes/carnes-crossroads-2015/src/App/Model/HomeFinderPage.php
+++ wp-content/themes/carnes-crossroads-2015/src/App/Model/HomeFinderPage.php
@@ -10,16 +10,16 @@ class HomeFinderPage
     {
         $current_user = User::getCurrentlyLoggedUser();
 
-        wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, true);
-        wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false);
-        wp_enqueue_style('slick-theme-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick-theme.css', array('slick-css'), false, false);
+//        wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, true);
+//        wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false);
+//        wp_enqueue_style('slick-theme-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick-theme.css', array('slick-css'), false, false);
         wp_enqueue_script('google-map-js', 'https://maps.google.com/maps/api/js', array(), false, true);
         wp_enqueue_script('multiple-select-js', get_template_directory_uri() . '/js/lib/multiple-select.js', array('jquery'), false, true);
         wp_enqueue_script('wnumb-js', get_template_directory_uri() . '/js/lib/wnumb/wNumb.js', array(), false, true);
         wp_enqueue_script('nouislider-js', get_template_directory_uri() . '/js/lib/nouislider/nouislider.min.js', array('wnumb-js'), false, true);
         wp_enqueue_style('nouislider-css', get_template_directory_uri() . '/js/lib/nouislider/nouislider.min.css');
         wp_enqueue_script('jquery-outside-events-js', get_template_directory_uri() . '/js/lib/jquery.ba-outside-events.min.js', array('jquery'), false, true);
-        wp_enqueue_script('home-finder-js', get_template_directory_uri() . '/js/home-finder.js', array('jquery', 'slick-js', 'backbone', 'nouislider-js', 'jquery-outside-events-js'), Config::getAppVersion(), true);
+        wp_enqueue_script('home-finder-js', get_template_directory_uri() . '/js/home-finder.js', array('jquery', 'backbone', 'nouislider-js', 'jquery-outside-events-js', 'multiple-select-js'), Config::getAppVersion(), true);
         wp_localize_script('home-finder-js', 'DI', array(
             'templateUri' => get_template_directory_uri(),
             'isLoggedIn' => ($current_user !== false) ? 'true' : 'false'
