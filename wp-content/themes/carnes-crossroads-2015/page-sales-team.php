faf0a695a5d89b2420171bf2a2d34c835efac310
Jeff <jeff.colfer@chernoffnewman.com>
2017-02-22 14:33:53 -0500

    Fix: office & mobile phone displaying on sales team page.

--- wp-content/themes/carnes-crossroads-2015/page-sales-team.php
+++ wp-content/themes/carnes-crossroads-2015/page-sales-team.php
@@ -1,14 +1,10 @@
 <?php
 
 use App\Model\RealEstateAgent;
-use \App\Model\Page;
 
 $context = Timber::get_context();
 $context['page'] = new TimberPost();
-$context['agent'] = RealEstateAgent::all();
-
-// $context['page'] = Timber::get_post(false, '');
-// $context['agent'] = RealEstateAgent::getContacts();
+$context['agents'] = RealEstateAgent::all();
 
 wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
 wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
