81e64ed34c3a51aca4433842352cf9c7359348eb
Danielle Salley <danielle.salley@chernoffnewman.com>
2017-03-17 09:58:22 -0400

    Part of the sales-team front end

---
+++ wp-content/themes/carnes-crossroads-2015/page-sales-team.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Model\RealEstateAgent;
+
+$context = Timber::get_context();
+$context['page'] = new TimberPost();
+$context['page'] = Timber::get_post(false, '\App\Model\Page');
+$context['agents'] = RealEstateAgent::all();
+
+wp_enqueue_script('slick-js', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array('jquery'), false, false);
+wp_enqueue_style('slick-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.css', array(), false, false);
+
+Timber::render('page-sales-team.twig', $context);
