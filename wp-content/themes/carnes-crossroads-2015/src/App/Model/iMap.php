2f725fb5b77ef4ea67b9d91abb61347f0b1fa627
Danielle Salley <danielle.salley@chernoffnewman.com>
2017-05-15 11:11:54 -0400

    primary-page.js not needed (pulled from DI, not on CX)

--- wp-content/themes/carnes-crossroads-2015/src/App/Model/iMap.php
+++ wp-content/themes/carnes-crossroads-2015/src/App/Model/iMap.php
@@ -13,10 +13,6 @@ class iMap
         wp_enqueue_style('slick-theme-css', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick-theme.css', array('slick-css'), false, false);
         wp_enqueue_script('waypoints-js', get_template_directory_uri() . '/js/lib/jquery.waypoints.min.js', array('jquery'), false, false);
         wp_enqueue_script('sticky-js', get_template_directory_uri() . '/js/lib/sticky.min.js', array('jquery'), false, false);
-        wp_enqueue_script('primary-page-js', get_template_directory_uri() . '/js/primary-page.js', array('jquery', 'slick-js', 'waypoints-js', 'sticky-js', 'backbone'), Config::getAppVersion(), true);
-        wp_localize_script('primary-page-js', 'DIRE', array(
-            'isRealEstatePage' => 'true'
-        ));
 
         //mapbox assets
         wp_enqueue_script('mapbox-js', 'https://api.mapbox.com/mapbox.js/v3.0.0/mapbox.js', array(), false, true);
