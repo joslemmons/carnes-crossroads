f51640a03cb55a2d24c0244221ca662c3bc4ed37
Danielle Salley <danielle.salley@chernoffnewman.com>
2017-06-16 16:28:33 -0400

    Add toggle view to POA calendar

--- wp-content/themes/carnes-crossroads-2015/page-events-activities.php
+++ wp-content/themes/carnes-crossroads-2015/page-events-activities.php
@@ -5,6 +5,16 @@ use App\Model\EventCalendar;
 
 global $params;
 
+//Event Calendar Styles
+wp_enqueue_style('tribe-events-pro-full-mobile-css', '/wp-content/plugins/events-calendar-pro/src/resources/css/tribe-events-pro-full-mobile.min.css');
+wp_enqueue_style('tribe-events-pro-full-css', '/wp-content/plugins/events-calendar-pro/src/resources/css/tribe-events-pro-full.min.css');
+wp_enqueue_style('widget-this-week-full-css', '/wp-content/plugins/events-calendar-pro/src/resources/css/widget-this-week-full.min.css');
+
+//Event Calendar Script
+wp_enqueue_script('widget-this-week-js', '/wp-content/plugins/events-calendar-pro/src/resources/js/widget-this-week.min.js', array('jquery'), false, true);
+wp_enqueue_script('tribe-events-bar-js', '/wp-content/plugins/the-events-calendar/src/resources/js/tribe-events-bar.min.js', array('jquery'), false, true);
+
+
 wp_enqueue_script('match-height-js', get_template_directory_uri() . '/js/lib/jquery.matchHeight-min.js', array('jquery'), false, false);
 wp_enqueue_script('poa-js', get_template_directory_uri() . '/js/poa.js', array('jquery', 'slick-js', 'match-height-js'), Config::getAppVersion(), true);
 
